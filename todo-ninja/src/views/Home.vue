244e5242638d6f4759c2005fe0cbb8765a18bbba
Brahim Bourhesdis <brahim.gx@outlook.fr>
2019-11-03 20:36:46 +0100

    first commit

---
+++ todo-ninja/src/views/Home.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="home">
+    <h1>homepage</h1>
+    <v-btn class="pink white--text">SIGN</v-btn>
+    <v-btn class="hidden-md-and-down">SIGN</v-btn>
+
+
+    <v-icon large color="teal darken-2">mdi-email</v-icon>
+    
+    <template>
+  <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+    >
+      <v-card-title>Top 10 Australian beaches</v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      <div>Whitehaven Beach</div>
+
+      <div>Whitsunday Island, Whitsunday Islands</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+        color="orange"
+        text
+      >
+        Share
+      </v-btn>
+
+      <v-btn
+        color="orange"
+        text
+      >
+        Explore
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<div id="app">
+  <v-container>
+    <v-layout row class="text-xs-center">
+      <v-flex xs3 style="background-image: url('http://cdn.wallpapersafari.com/7/86/gqiGH7.jpg')">
+        <v-card height="500px"></v-card>
+      </v-flex>
+      <v-flex xs4 class="grey lighten-4">
+        <v-container style="position: relative;top: 13%;" class="text-xs-center">
+          <v-card flat>
+            <v-card-title primary-title>
+              <h4>Login</h4>
+            </v-card-title>
+            <v-form>
+            <v-text-field prepend-icon="person" name="Username" label="Username"></v-text-field>
+            <v-text-field prepend-icon="lock" name="Password" label="Password" type="password"></v-text-field>
+            <v-card-actions>
+              <v-btn primary large block>Login</v-btn>
+            </v-card-actions>
+            </v-form>
+          </v-card>
+        </v-container>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</div>
+     
+  </div>
+</template>
+
+
+
+<script>
+// @ is an alias to /src
+export default {
+
+}
+</script>
